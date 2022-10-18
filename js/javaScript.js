a7edfb10472ef0020b33e533663d5360414376e5
Haolin Song <72663301+ethanshl@users.noreply.github.com>
2022-10-18 16:21:44 -0400

    Working on time validation

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,5 +1,7 @@
+
+
 function storeTimeGarage() {
-    var garage = document.getElementById("garageSelect").value;
+  var garage = document.getElementById("garageSelect").value;
 	var dateString = document.getElementById("startTime").value;
 	var dateString2 = document.getElementById("endTime").value;
 
@@ -9,26 +11,38 @@ function storeTimeGarage() {
 	var startTime =  DateStart.toLocaleTimeString();
 	var endDate =  DateEnd.toLocaleDateString();
 	var endTime =  DateEnd.toLocaleTimeString();
-  
-    var startDateTime = startDate + " " + startTime;
-    var endDateTime = endDate + " " + endTime;
+
+  var startDateTime = startDate + " " + startTime;
+  var endDateTime = endDate + " " + endTime;
 
 	localStorage.setItem("garage", garage);
 	localStorage.setItem("startDateTime", startDateTime);
 	localStorage.setItem("endDateTime", endDateTime);
-	
-	
+
+
 };
 
 function checkDate() {
-	var dateString = document.getElementById("startTime").value;
-	var dateString2 = document.getElementById("endTime").value;
-	var DateStart = new Date(dateString);
-	var DateEnd = new Date(dateString2);
-	if (DateEnd < DateStart) {
+  var dateString1 = document.getElementById("startDate").value;
+  var dateString2 = document.getElementById("endDate").value;
+	var dateString3 = document.getElementById("startTime").value;
+	var dateString4 = document.getElementById("endTime").value;
+
+	var dateStart = new Date(dateString1);
+  var timeStart = new Date(dateString3);
+  var dateEnd = new Date(dateString2);
+  var timeEnd = new Date(dateString4);
+
+	if (dateEnd < dateStart) {
 		alert("End date cannot be less than Start date.");
-		document.getElementById('#endTime').value = "";
+		document.getElementById('endDate').value = "";
 		return false;
-	}
+	}else if(dateEnd = dateStart){
+    if(timeEnd < timeStart) {
+      alert("End time cannot be less than Start time.");
+      document.getElementById('endTime').value = "";
+      return false;
+    }
+  }
 	return true;
 };
